75e616d6b406d6bb7b392c45e9f735bf1cdac620
Shushuda <abcusca@gmail.com>
2019-04-04 03:41:48 +0200

    Add exception handling for connections

--- character.py
+++ character.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-
 import requests
 from utils import convert_race_name, convert_faction_name, convert_class_name, convert_gender_name, \
     get_character_thumbnail, fix_realm_name
+from exceptions import APIConnectionError
 
 
 class Character:
@@ -38,12 +38,17 @@ class Character:
 
     @staticmethod
     def get_character_profile(access_token, region, realm, character_name):
-        # TODO
-        # make an error class with exception handling (connectionError etc)
-        # make the app throw those errors outside of the app for the end-user to handle themselves
-        response = requests.get(
-            f'https://{region}.api.blizzard.com/wow/character/{realm}/{character_name}?fields=items%2C%20guild%2C%20appearance%2C%20achievements%2C%20feed%2C%20mounts&access_token={access_token}'
-        )
+        try:
+            response = requests.get(
+                f'https://{region}.api.blizzard.com/wow/character/{realm}/{character_name}?fields=items%2C%20guild%2C%20appearance%2C%20achievements%2C%20feed%2C%20mounts&access_token={access_token}'
+            )
+        except requests.exceptions.ConnectionError as e:
+            raise APIConnectionError(e)
+
+        if response.status_code == 401:
+            e = "Invalid access token"
+            raise APIConnectionError(e)
+
         return response
 
     def get_last_modified_string(self):
