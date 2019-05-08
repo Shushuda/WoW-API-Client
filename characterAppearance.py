9e43076678d8b972e8e5561820668836e9bee3ec
Shushuda <abcusca@gmail.com>
2019-05-08 22:21:30 +0200

    Add small refactor

--- characterAppearance.py
+++ characterAppearance.py
@@ -1,26 +1,29 @@
+from utils import deep_get
+
+
 def get_face_type(self):
-    return self.character_profile['appearance']['faceVariation']
+    return deep_get(self.character_profile, 'appearance', 'faceVariation')
 
 
 def get_skin_color(self):
-    return self.character_profile['appearance']['skinColor']
+    return deep_get(self.character_profile, 'appearance', 'skinColor')
 
 
 def get_hair_type(self):
-    return self.character_profile['appearance']['hairVariation']
+    return deep_get(self.character_profile, 'appearance', 'hairVariation')
 
 
 def get_hair_color(self):
-    return self.character_profile['appearance']['hairColor']
+    return deep_get(self.character_profile, 'appearance', 'hairColor')
 
 
 def get_feature_type(self):
-    return self.character_profile['appearance']['featureVariation']
+    return deep_get(self.character_profile, 'appearance', 'featureVariation')
 
 
 def get_show_helm(self):
-    return self.character_profile['appearance']['showHelm']
+    return deep_get(self.character_profile, 'appearance', 'showHelm')
 
 
 def get_show_cloak(self):
-    return self.character_profile['appearance']['showCloak']
+    return deep_get(self.character_profile, 'appearance', 'showCloak')
--- exceptions.py
+++ exceptions.py
@@ -2,5 +2,5 @@ class APIConnectionError(Exception):
 
     def __init__(self, original_exception):
         msg = "Unable to connect"
-        super().__init__(msg + (": %s" % original_exception))
+        super().__init__(msg + f": {original_exception}")
         self.original_exception = original_exception
